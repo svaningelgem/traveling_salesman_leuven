bfcc0d3adab08338dea50124fb4b25e6808627b7
Steven Van Ingelgem <steven@vaningelgem.be>
2026-09-16 15:41:06 +0200

    style: ruff format and autofix

--- common.py
+++ common.py
@@ -1,17 +1,17 @@
 import contextlib
 import csv
 import math
+from collections.abc import Generator
 from datetime import datetime
-from typing import Generator, Tuple
 
 import numpy as np
 from scipy import spatial
 
-Point = Tuple[float, float]
+Point = tuple[float, float]
 
 
-def _load_data() -> Generator[Tuple[float, float], None, None]:
-    with open('coordinates.csv', 'r') as fp:
+def _load_data() -> Generator[tuple[float, float], None, None]:
+    with open("coordinates.csv", "r") as fp:
         reader = csv.reader(fp)
         next(reader, None)  # Skip header
         for line in reader:
@@ -21,29 +21,25 @@ def _load_data() -> Generator[Tuple[float, float], None, None]:
 def distance(x: Point, y: Point) -> float:
     # https://www.cuemath.com/euclidean-distance-formula/
     # I know I should use the haversine distance here, but the curve of the earth ain't that wrong on like 10km...
-    return math.sqrt(
-        (x[0] - x[1]) ** 2
-        + (y[0] - y[1]) ** 2
-    )
+    return math.sqrt((x[0] - x[1]) ** 2 + (y[0] - y[1]) ** 2)
 
 
 coordinates = np.array(list(_load_data()))
 num_points = len(coordinates)
-distance_matrix = spatial.distance.cdist(coordinates, coordinates, metric='euclidean')
+distance_matrix = spatial.distance.cdist(coordinates, coordinates, metric="euclidean")
 
 
 def write_gps_file(coords: np.ndarray, name):
-    with open(f'{name}.gpx', 'w') as fp:
+    with open(f"{name}.gpx", "w") as fp:
         fp.write('<?xml version="1.0" encoding="utf-8"?>\n')
         fp.write('<gpx version="1.1" creator="Me, myself and I">\n')
-        fp.write('  <rte>\n')
-        fp.write(f'    <name>{name}</name>\n')
-        fp.write('    <number>0</number>\n')
+        fp.write("  <rte>\n")
+        fp.write(f"    <name>{name}</name>\n")
+        fp.write("    <number>0</number>\n")
 
-        for coord in coords:
-            fp.write(f'    <rtept lat="{coord[0]}" lon="{coord[1]}"/>\n')
-        fp.write('  </rte>\n')
-        fp.write('</gpx>\n')
+        fp.writelines(f'    <rtept lat="{coord[0]}" lon="{coord[1]}"/>\n' for coord in coords)
+        fp.write("  </rte>\n")
+        fp.write("</gpx>\n")
 
 
 def default_route():
@@ -65,4 +61,6 @@ def check_time():
 
 
 def cost(route):
-    return distance_matrix[np.roll(route, 1), route].sum()  # shifts route array by 1 in order to look at pairs of cities
+    return distance_matrix[
+        np.roll(route, 1), route
+    ].sum()  # shifts route array by 1 in order to look at pairs of cities
